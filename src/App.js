3c9a65d07799940f27f790ed8cb5994975de6da2
Geraldy <geraldy.miero@gmail.com>
2021-01-31 14:32:25 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import './App.css';
+
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="c5ce77a8-7ae6-4f80-b91d-023305fec2bc"
+            userName="archie"
+            userSecret="123"
+        />
+    );
+}
+
+export default App;
