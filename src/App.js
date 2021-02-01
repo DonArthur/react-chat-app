f4cd19e03158cc1c877d2f4c1665bc0b8d5ed713
Geraldy <geraldy.miero@gmail.com>
2021-02-02 00:45:03 +0700

    Add message components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ChatEngine } from 'react-chat-engine';
-
+import ChatFeed from './components/ChatFeed';
 import './App.css';
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
             projectID="c5ce77a8-7ae6-4f80-b91d-023305fec2bc"
             userName="archie"
             userSecret="123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     );
 }
